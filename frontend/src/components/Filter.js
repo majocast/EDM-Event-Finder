eb53734ba3bdd7e44ba43041aa5cc95e787bc5fa
Marc Joseph Guerrero Castro <marcjgcastro@gmail.com>
2023-09-05 13:53:58 -0700

    setup basic filter system

---
+++ frontend/src/components/Filter.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Filter = ( {data, onDataFiltered }) => {
+  const [filterText, setFilteredText] = useState('');
+
+  const handleFilterChange = (e) => {
+    const searchText = e.target.value;
+    setFilteredText(searchText);
+
+
+    const filteredData = data.filter(item => 
+      item.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      item.location.toLowerCase().includes(searchText.toLowerCase()) ||
+      item.date.toLowerCase().includes(searchText.toLowerCase()) 
+    );
+
+    onDataFiltered(filteredData);
+  };
+
+
+
+  return (
+    <div className="filter">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Filter data..."
+        value={filterText}
+        onChange={handleFilterChange}
+      />
+    </div>
+  )
+}
+
+export default Filter;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Event from '../components/Event';
+import Filter from '../components/Filter';
 import { Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 const Events = (props) => {
   const data = props.myData;
+  const [filteredData, setFilteredData] = useState(data.data);
+
+  const handleDataFiltered = (filteredData) => { 
+    setFilteredData(filteredData); 
+  }
+
   return (
-    <div className='events'>
-      <div className='filter'>
-        <h1>Filter</h1>
-      </div>
-      <div>
-        <Row>
-          {data.data.map((item, index) => {
-            const event = new Event(item.title, item.date, item.location, item.link, item.photo, index);
-            return (
-              <Col key={index} xs={12} sm={6} md={4}>
-                {event.display()}
-              </Col>
-            )
-          })}
-        </Row>
-      </div>
-    </div>
+    <Container className='events'>
+      <Row>
+        <Col sm={4} className='filterCol'>
+          <Filter data={data.data} onDataFiltered={handleDataFiltered}/>
+        </Col>
+        <Col sm={8} className='eventTable'>
+          <Row>
+            {filteredData.map((item, index) => {
+              const event = new Event(item.title, item.date, item.location, item.link, item.photo, index);
+              return (
+                <Col key={index} xs={12} sm={6} md={4}>
+                  {event.display()}
+                </Col>
+              )
+            })}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
