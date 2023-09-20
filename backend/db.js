5487467ec308a5c98871f32430bc4fb8211c3047
Marc Joseph Guerrero Castro <marcjgcastro@gmail.com>
2023-09-19 19:47:44 -0700

    reverted env vars for merge with prod

--- backend/db.js
+++ backend/db.js
@@ -2,16 +2,14 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  password: process.env.LOCALDBPASS,
-  host: 'localhost',
-  port: 5432,
-  database: 'edmeventfinder',
-  /*
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  database: process.env.DATABASE,
   ssl: {
     rejectedUnauthorized: false,
   },
-  */
 });
 
 /*
