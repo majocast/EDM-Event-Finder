1bcfac3bf1a804a2294f04ce4bce51b4d0197399
majocast <marcjgcastro@gmail.com>
2023-10-01 15:40:05 -0700

    changed home screen and nav text styles

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { MdSwitchAccount } from 'react-icons/md';
 
 const NavBar = () => {
   const location = useLocation();
